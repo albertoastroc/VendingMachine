07fac43f18a0b7d3f35c3ffba58ee0de666bdc06
Eric Cameron <eric.cameron@techelevator.com>
2023-11-30 12:04:54 -0500

    Initial commit

---
+++ src/main/java/com/techelevator/VendingMachineCLI.java
@@ -0,0 +1,21 @@
+package com.techelevator;
+
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+/*
+ * This class is provided to you as a *suggested* class to start
+ * your project. Feel free to refactor this code as you see fit.
+ */
+public class VendingMachineCLI {
+
+	public static void main(String[] args) {
+		VendingMachineCLI cli = new VendingMachineCLI();
+		cli.run();
+	}
+
+	public void run() {
+
+	}
+
+}
